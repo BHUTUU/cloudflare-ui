c549b496f182a38933d615a5c063687c1a101731
BHUTUU <raj259942@gmail.com>
2021-11-14 11:15:50 +0530

    a bug fixed

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-arg1="$1"
-arg2="$2"
 printf "\033[32mSetting-up cloudflare in your system\033[00m\n"
 cd $PREFIX/share
 git clone https://github.com/BHUTUU/cloudflare-ui
 cat <<- VAR > $PREFIX/bin/cloudflare
 #!/bin/bash
+arg1="\$1"
+arg2="\$2"
 cd $PREFIX/share/cloudflare-ui
-bash cloudflare ${arg1} ${arg2}
+bash cloudflare \${arg1} \${arg2}
 VAR
 chmod +x $PREFIX/bin/cloudflare
 printf "\n\nConfiguration completed just run 'clouflare --help' for help\n\n"
