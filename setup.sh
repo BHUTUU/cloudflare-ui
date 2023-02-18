2c8049b8a82e695f8525b92860db08c848b20534
BHUTUU <raj259942@gmail.com>
2023-02-18 12:43:40 +0530

    updated setup file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-ping -c 1 google.com >/dev/null 2>&1
+OS=$(uname -o)
+if [[ ${OS^^} == *'MSYS'* || ${OS^^} == *'WINDOWS'* ]]; then
+  PREFIX="/usr"
+  adminPerm=$(net user administrator | grep active | awk '{print $NF}')
+  case ${adminPerm,,} in
+    yes) opposite="no";;
+    no) opposite="yes";;
+    *) printf "Not possible to run this\n"; exit 1;;
+  esac
+  net user administrator /active:${opposite} >/dev/null 2>&1
+  adminPerm=$(net user administrator | grep active | awk '{print $NF}')
+  if [[ ${adminPerm,,} != "${opposite}" ]]; then
+    printf "${S2}[${S1}!${S2}]${S4}Run this command prompt or shell in Administrator mode${R0}\n"
+    exit 1
+  fi
+fi
+curl -s google.com >/dev/null 2>&1
 if [ "$?" != '0' ]; then
     printf "\033[1;2;4;5;32m[\033[31m!\033[32m] \033[34mCheck your internet connection!\033[0;0;0;0;00m\n"
     exit 1
@@ -19,8 +35,9 @@ cat <<- VAR > $PREFIX/bin/cloudflare
 #!/bin/bash
 arg1="\$1"
 arg2="\$2"
+arg3="\$3"
 cd $PREFIX/share/cloudflare-ui
-bash cloudflare \${arg1} \${arg2}
+bash cloudflare \${arg1} \${arg2} \${arg3}
 VAR
 chmod +x $PREFIX/bin/cloudflare
 printf "\n\nConfiguration completed just run 'clouflare --help' for help\n\n"
